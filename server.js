37a090301cf2658f5e44c88555acaa650f61c62d
RolandOroz <rolandoroz@gmail.com>
2023-05-19 05:59:29 +0200

    express middleware exmpl

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
-
-//*_Server setup_S
 const PORT = process.env.PORT || 3500;
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.google.com",
+  "https://www.yoursiteOrYourDomainFromFE.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    //set to no origin(!origin) only in DEV MODE!!
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// built-in middleware to handle URL encoded data
+// in other words, form data:
+// "content-type: application/x-www-form-urlencoded"
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //res.sendFile("./views/index.html", { root: __dirname });
@@ -44,10 +79,19 @@ const three = (req, res, next) => {
   res.send("finished");
 };
 app.get("/chain(.html)?", [one, two, three]);
-//_Chaining Route Handlers_E
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found!" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+// custom error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
